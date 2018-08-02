49aa30415395572088d371432b19defc9b62470a
genshen <genshenchu@gmail.com>
2018-08-03 01:21:37 +0800

    feat(test): add MPIEnvironment class for mpi test under googletest.

--- test/utils/gtest_env.h
+++ test/utils/gtest_env.h
@@ -5,4 +5,24 @@
 #ifndef KIWI_GTEST_ENV_H
 #define KIWI_GTEST_ENV_H
 
+#include <gtest/gtest.h>
+#include <utils/mpi_utils.h>
+
+namespace kiwi {
+    class MPIEnvironment : public ::testing::Environment {
+    public:
+        void SetUp() override {
+            char **argv = nullptr;
+            int argc = 0;
+            kiwi::mpiUtils::initialMPI(argc, argv);
+        }
+
+        void TearDown() override {
+            kiwi::mpiUtils::finishMPI();
+        }
+
+        ~MPIEnvironment() override = default;
+    };
+}
+
 #endif //KIWI_GTEST_ENV_H
